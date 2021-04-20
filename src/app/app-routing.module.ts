af0bbb3a5224739efb614a9e6f9a06c9c582856d
Ali-javed04 <58926148+Ali-javed04@users.noreply.github.com>
2021-04-20 09:41:52 +0500

    ali commmit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { MaxComponent } from './components/max/max.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'' ,component:HomeComponent },
+  { path:'max',component:MaxComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
